4bd524948a96aee15ab1f8d1a18e366dc932eca9
Amir Aryani <amir.aryani@gmail.com>
2017-10-09 15:48:26 +1100

    fixed import-xml settings

--- jobs/registry/ingest-figshare.sh
+++ jobs/registry/ingest-figshare.sh
@@ -11,7 +11,6 @@ cp -r $GITLOCALFOLDER/Import-XML/target/* .
 echo "Get the crosswalks from github.."
 curl https://raw.githubusercontent.com/researchgraph/Crosswalks/master/figshare.com/figshareRDF_to_researchgraph.xsl > ./crosswalk.xsl
 
-SOURCE=figshare
 
 mkdir ./versions
 mkdir ./versions/figshare
@@ -22,6 +21,9 @@ JARFILE=import-xml-1.3.6.jar
 VERBOSE=false #for production
 #VERBOSE=true #for testing and debuging 
 
+NOW="$(date +'%Y-%m-%d')"
+
+SOURCE=monash.figshare
 java -jar $JARFILE \
-  -n ./R.Neo4j/ -s $SOURCE -f ./monash \
+  -n ./R.Neo4j/ -s $SOURCE -f ./xml/figshare/rdf/$NOW/portal_21 \
   -C crosswalk.xsl -v ./versions -V "$VERBNOSE"
