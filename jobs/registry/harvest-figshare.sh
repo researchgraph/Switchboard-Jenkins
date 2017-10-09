4ba6eca8e0c3f720a8b20f216e7b3beb7a63350c
Amir Aryani <amir.aryani@gmail.com>
2017-10-09 13:34:21 +1100

    removed sudo

--- jobs/registry/harvest-figshare.sh
+++ jobs/registry/harvest-figshare.sh
@@ -1,8 +1,8 @@
 GITLOCALFOLDER=$1
 
 echo "Copy the harvester jar file"
-sudo cp -r $GITLOCALFOLDER/Harvesters/OAI_PMH/harvester_oai/target/* .
+cp -r $GITLOCALFOLDER/Harvesters/OAI_PMH/harvester_oai/target/* .
 
 JARFILE=harvester_oai-1.3.8.jar
 
-sudo java -jar $JARFILE harvest-figshare.properties
+java -jar $JARFILE harvest-figshare.properties
