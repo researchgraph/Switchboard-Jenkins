c0ccd25fb0a15819d19a0670c1a7c631966c22c5
Amir Aryani <amir.aryani@gmail.com>
2017-10-16 15:52:52 +1000

    removed neo4j folder

--- jobs/synthesis/synthesis-monash-figshare.sh
+++ jobs/synthesis/synthesis-monash-figshare.sh
@@ -6,12 +6,7 @@ aws s3 --region us-west-2 sync s3://jenkins.switchboard.researchgraph/ .
 echo "Get the Synthesis Program..."
 sudo cp -r $GITLOCALFOLDER/Synthesis/target/* .
 
-#echo "Create clean Neo4j Folder..."
-#rm -rf ./R.Neo4j
-#cp -r $GITLOCALFOLDER/R.Neo4j .
-
-
 JARFILE=sync-1.0.6.jar
 
-#sudo java -jar $JARFILE synthesis-monash-figshare.properties
+sudo java -jar $JARFILE synthesis-monash-figshare.properties
 
