7aa53e079c18eb4560a4b4755a689afaa5f19515
Amir Aryani <amir.aryani@gmail.com>
2017-10-17 09:22:23 +1000

    removed sudo

--- jobs/synthesis/synthesis-monash-figshare.sh
+++ jobs/synthesis/synthesis-monash-figshare.sh
@@ -1,12 +1,12 @@
 GITLOCALFOLDER=$1
 
 echo "Get the Jenkins files"
-aws s3 --region us-west-2 sync s3://jenkins.switchboard.researchgraph/jobs/synthesis/* .
+aws s3 --region us-west-2 sync s3://jenkins.switchboard.researchgraph/jobs/synthesis .
 
 echo "Get the Synthesis Program..."
-sudo cp -r $GITLOCALFOLDER/Synthesis/target/* .
+cp -r $GITLOCALFOLDER/Synthesis/target/* .
 
 JARFILE=sync-1.0.6.jar
 
-sudo java -jar $JARFILE synthesis-monash-figshare.properties
+java -jar $JARFILE synthesis-monash-figshare.properties
 
