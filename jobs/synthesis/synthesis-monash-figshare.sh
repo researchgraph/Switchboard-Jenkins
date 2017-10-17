d6c445dce31fc41846c43b59f88c2605a3cf2472
Amir Aryani <amir.aryani@gmail.com>
2017-10-17 14:12:57 +1000

    break the S3 bucket connection

--- jobs/synthesis/synthesis-monash-figshare.sh
+++ jobs/synthesis/synthesis-monash-figshare.sh
@@ -1,7 +1,7 @@
 GITLOCALFOLDER=$1
 
-echo "Get the Jenkins files"
-aws s3 --region us-west-2 sync s3://jenkins.switchboard.researchgraph/jobs/synthesis .
+#echo "Get the Jenkins files"
+#aws s3 --region us-west-2 sync s3://jenkins.switchboard.researchgraph/jobs/synthesis .
 
 echo "Get the Synthesis Program..."
 cp -r $GITLOCALFOLDER/Synthesis/target/* .
