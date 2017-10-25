531b94cdae5ac4a8705141d831d4f869c4910afd
Amir Aryani <amir.aryani@gmail.com>
2017-10-25 22:47:25 +1100

    debuging ...

--- jobs/registry/harvest-nci.sh
+++ jobs/registry/harvest-nci.sh
@@ -8,16 +8,16 @@ cp -r $GITLOCALFOLDER/R.Neo4j .
 rm -rf csv
 mkdir csv
 echo "Download CSV files..."
-sudo curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/datasets.csv > ./csv/datasets.csv
-sudo curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/researchers.csv > ./csv/researchers.csv
-sudo curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/relations.csv > ./csv/relations.csv
+curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/datasets.csv > ./csv/datasets.csv
+curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/researchers.csv > ./csv/researchers.csv
+curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/relations.csv > ./csv/relations.csv
 
 echo "Building Neo4j ... "
-sudo ./R.Neo4j/bin/neo4j-import --into ./R.Neo4j/data/databases/graph.db \
+./R.Neo4j/bin/neo4j-import --into ./R.Neo4j/data/databases/graph.db \
   --nodes ./csv/researcher.csv \
-  --nodes ./csv/datasets.csv \
   --relationships ./csv/relations.csv \
   --ignore-empty-strings true
 
-sudo zip "r-neo4j-nci.zip" ./R.Neo4j
-aws s3 --region eu-west-2 cp "r-neo4j-nci.zip"  s3://nci.registry.researchgraph/neo4j/
+ # --nodes ./csv/datasets.csv \
+#sudo zip "r-neo4j-nci.zip" ./R.Neo4j
+#aws s3 --region eu-west-2 cp "r-neo4j-nci.zip"  s3://nci.registry.researchgraph/neo4j/
