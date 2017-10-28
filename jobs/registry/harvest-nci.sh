dc9640458aff1944edc49d708ca4015c543ed451
Amir Aryani <amir.aryani@gmail.com>
2017-10-28 19:50:50 +1100

    Added publications

--- jobs/registry/harvest-nci.sh
+++ jobs/registry/harvest-nci.sh
@@ -10,12 +10,14 @@ mkdir csv
 echo "Download CSV files..."
 curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/datasets.csv > ./csv/datasets.csv
 curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/researchers.csv > ./csv/researchers.csv
+curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/publications.csv > ./csv/publications.csv
 curl https://raw.githubusercontent.com/researchgraph/nci/master/csv/relations.csv > ./csv/relations.csv
 
 echo "Building Neo4j ... "
 ./R.Neo4j/bin/neo4j-import --into ./R.Neo4j/data/databases/graph.db \
   --nodes ./csv/datasets.csv \
   --nodes ./csv/researchers.csv \
+  --nodes ./csv/publications.csv \
   --relationships ./csv/relations.csv \
   --ignore-empty-strings true
 
