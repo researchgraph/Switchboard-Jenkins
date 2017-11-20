9da6ae92c54a05605288f50471bba9377dc68cf1
Amir Aryani <amir.aryani@gmail.com>
2017-11-20 13:23:41 +1100

    First versions of the Build-GraphML-NCI

---
+++ jobs/registry/build-graphml-nci.sh
@@ -0,0 +1,17 @@
+cp /mnt/jenkins/workspace/Register-NCI/jobs/registry/R.Neo4j .
+cd R.Neo4j
+curl http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip -o neo4j-shell-tools.zip
+unzip neo4j-shell-tools.zip -d lib
+
+chmod +x ./bin/neo4j-shell
+
+echo "Starting Neo4j"
+./bin/neo4j start
+
+sleep 10s
+
+echo "Exporting GraphML using Neo4j-Shell"
+./bin/neo4j-shell -c -file  ../build-nci-graphml.cli
+
+echo "Stopping Neo4j"
+./bin/neo4j stop
