e7576ed2370e696f877300c9585ee7daa0fe633b
Amir Aryani <amir.aryani@gmail.com>
2017-11-20 15:27:47 +1100

    debugging

--- jobs/registry/build-graphml-nci.sh
+++ jobs/registry/build-graphml-nci.sh
@@ -1,7 +1,9 @@
 NCINEO4J=$1
-cp $NCINEO4J .
 
-cd R.Neo4j
+rm -rf ./R.Neo4j
+cp -r $NCINEO4J .
+
+cd ./R.Neo4j
 curl http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip -o neo4j-shell-tools.zip
 unzip neo4j-shell-tools.zip -d lib
 
