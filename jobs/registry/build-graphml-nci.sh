87ca18deef5747788eb320855c8be68c874347ae
Amir Aryani <amir.aryani@gmail.com>
2017-11-20 15:30:39 +1100

    Debugging

--- jobs/registry/build-graphml-nci.sh
+++ jobs/registry/build-graphml-nci.sh
@@ -4,18 +4,18 @@ rm -rf ./R.Neo4j
 cp -r $NCINEO4J .
 
 cd ./R.Neo4j
+echo "Downloading neo4j-shell-tools"
 curl http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip -o neo4j-shell-tools.zip
-unzip neo4j-shell-tools.zip -d lib
+unzip -f neo4j-shell-tools.zip -d lib
 
 chmod +x ./bin/neo4j-shell
 
-echo "Starting Neo4j"
 ./bin/neo4j start
-
 sleep 15s
+
+./bin/neo4j status
 
 echo "Exporting GraphML using Neo4j-Shell"
 ./bin/neo4j-shell  -v -c -file  ../build-graphml-nci.cli
 
-echo "Stopping Neo4j"
 ./bin/neo4j stop
