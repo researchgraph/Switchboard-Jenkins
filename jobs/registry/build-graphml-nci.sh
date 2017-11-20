f8936eba67feb5bfd5fd971a9df80cf53771eb2f
Amir Aryani <amir.aryani@gmail.com>
2017-11-20 14:08:07 +1100

    No need to copy Neo4j anymore
    Merging build Jenkins job with registry job.

--- jobs/registry/build-graphml-nci.sh
+++ jobs/registry/build-graphml-nci.sh
@@ -1,4 +1,3 @@
-cp /mnt/jenkins/workspace/Register-NCI/jobs/registry/R.Neo4j .
 cd R.Neo4j
 curl http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip -o neo4j-shell-tools.zip
 unzip neo4j-shell-tools.zip -d lib
