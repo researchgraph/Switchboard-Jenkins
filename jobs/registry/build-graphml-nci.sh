b5cf78039aba68918efb1d11d1e2b5f656c810c2
Amir Aryani <amir.aryani@gmail.com>
2017-11-20 16:13:23 +1100

    Removed downloading Neo4j Shell Tools

--- jobs/registry/build-graphml-nci.sh
+++ jobs/registry/build-graphml-nci.sh
@@ -4,11 +4,6 @@ rm -rf ./R.Neo4j
 cp -r $NCINEO4J .
 
 cd ./R.Neo4j
-echo "Downloading neo4j-shell-tools"
-curl http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip -o neo4j-shell-tools.zip
-unzip -f neo4j-shell-tools.zip -d lib
-
-chmod +x ./bin/neo4j-shell
 
 ./bin/neo4j start
 sleep 15s
