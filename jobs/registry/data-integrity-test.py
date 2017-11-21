2d33051090992b5e563d33b16065446673dd8c93
Amir Aryani <amir.aryani@gmail.com>
2017-11-21 18:24:33 +1100

    added new files

---
+++ jobs/registry/data-integrity-test.py
@@ -0,0 +1,52 @@
+import sys
+# from py2neo import Node, Relationship
+from py2neo import Graph
+
+valid_labels = ['researcher', 'dataset', 'publication', 'grant', 'axillary', 'nci']
+valid_types = ['researcher', 'dataset', 'publication', 'grant', 'axillary']
+
+
+def main(source_label):
+    error_count = 0
+    print "Source Label: '%s' " % source_label
+
+    graph = Graph()
+
+    # Check if all nodes have valid Type Label
+    query = 'MATCH (n) where NOT n:researcher and NOT n:publication and NOT n:dataset and NOT n:grant RETURN count(n)'
+    result=graph.run(query).evaluate()
+    if result>0:
+        error_count +=1
+        print ("Error %d: %d nodes are missing at least one Valid Type Label "
+               "('researcher', 'dataset', 'publication', 'grant')" % (error_count, result))
+
+    # Check of if all labels are valid
+    query = 'MATCH (n) RETURN distinct(labels(n)) as Labels'
+    cursor= graph.run(query)
+    while cursor.forward():
+        labels=cursor.current()['Labels']
+        for l in labels:
+            if l not in valid_labels:
+                    error_count += 1
+                    print "Error %d: '%s' is an invalid Label in this graph cluster" % (error_count,l)
+
+    # Check if all nodes have valid 'type' value
+    query = "MATCH (n) where NOT n.type='%s' and NOT n.type='%s' " \
+            "and NOT n.type='%s' and NOT n.type='%s' RETURN count(n)" % tuple(valid_types)
+    result = graph.run(query).evaluate()
+    if result > 0:
+        error_count += 1
+        print ("Error %d: %d nodes are missing at least one valid n.type value "
+               "('researcher', 'dataset', 'publication', 'grant')" % (error_count, result))
+
+    if error_count>0:
+        raise Exception('This graph cluster is not valid!')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
+# for record in graph.run(query):
+# print(record[0])
+
+
