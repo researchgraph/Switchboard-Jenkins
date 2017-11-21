c57bda3ce07bfba0abb392fd87ea275c6795e7bc
Amir Aryani <amir.aryani@gmail.com>
2017-11-21 23:15:44 +1100

    added the data-integrity-test.sh

---
+++ jobs/registry/data-integrity-test.sh
@@ -0,0 +1,15 @@
+NCINEO4J=$1
+SOURCE=$2
+
+rm -rf ./R.Neo4j
+cp -r $NCINEO4J .
+
+./R.Neo4j/bin/neo4j start
+sleep 15s
+
+./R.Neo4j/bin/neo4j status
+
+echo "Testing the data integrity"
+python data-integrity-test.sh $SOURCE
+
+./R.Neo4j/bin/neo4j stop
