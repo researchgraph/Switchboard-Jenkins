3579df1199875b93fa99ab6c54382b80ca9adbdd
Amir Aryani <amir.aryani@gmail.com>
2017-11-22 00:35:49 +1100

    added new test

--- jobs/registry/data-integrity-test.py
+++ jobs/registry/data-integrity-test.py
@@ -13,40 +13,44 @@ def main(source_label):
     graph = Graph()
 
     # Check if all nodes have valid Type Label
+    query = 'match (n) where labels(n)=[] return count(n)'
+    result = graph.run(query).evaluate()
+    if result > 0:
+        error_count += 1
+        print ("Error %d: %d nodes have no label!" % (error_count, result))
+        return
+
+    # Check if all nodes have valid Type Label
     query = 'MATCH (n) where NOT n:researcher and NOT n:publication and NOT n:dataset and NOT n:grant RETURN count(n)'
-    result=graph.run(query).evaluate()
-    if result>0:
-        error_count +=1
+    result = graph.run(query).evaluate()
+    if result > 0:
+        error_count += 1
         print ("Error %d: %d nodes are missing at least one Valid Type Label "
                "('researcher', 'dataset', 'publication', 'grant')" % (error_count, result))
 
     # Check of if all labels are valid
     query = 'MATCH (n) RETURN distinct(labels(n)) as Labels'
-    cursor= graph.run(query)
+    cursor = graph.run(query)
     while cursor.forward():
-        labels=cursor.current()['Labels']
+        labels = cursor.current()['Labels']
         for l in labels:
             if l not in valid_labels:
                     error_count += 1
                     print "Error %d: '%s' is an invalid Label in this graph cluster" % (error_count,l)
 
     # Check if all nodes have valid 'type' value
-    query = "MATCH (n) where NOT n.type='%s' and NOT n.type='%s' " \
-            "and NOT n.type='%s' and NOT n.type='%s' RETURN count(n)" % tuple(valid_types)
+    query = "MATCH (n) where NOT (n.type in %s) RETURN count(n)" % valid_types
     result = graph.run(query).evaluate()
     if result > 0:
         error_count += 1
         print ("Error %d: %d nodes are missing at least one valid n.type value "
                "('researcher', 'dataset', 'publication', 'grant')" % (error_count, result))
 
-    if error_count>0:
+    if error_count > 0:
         raise Exception('This graph cluster is not valid!')
 
 
 if __name__ == "__main__":
     main(sys.argv[1])
-
-# for record in graph.run(query):
-# print(record[0])
 
 
