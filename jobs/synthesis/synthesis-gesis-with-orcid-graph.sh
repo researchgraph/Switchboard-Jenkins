d305cc4fb03cf3925b17b20e9291017194a997d7
Amir Aryani <amir.aryani@gmail.com>
2017-12-19 22:18:07 +1100

    created synthesis-gesis properties file

--- jobs/synthesis/synthesis-gesis-with-orcid-graph.sh
+++ jobs/synthesis/synthesis-gesis-with-orcid-graph.sh
@@ -3,5 +3,4 @@ cp -r $JENKINS_HOME/jobs/Build-Switchboard-Synthesis/workspace/target/* .
 
 JARFILE=sync-1.1.2.jar
 
-java -jar $JARFILE synthesis-base.properties
-
+java -jar $JARFILE synthesis-gesis-with-orcid-graph.properties
