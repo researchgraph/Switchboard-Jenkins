fe7afd770faee88eba6e5a547cddcc4696a155bc
Amir Aryani <amir.aryani@gmail.com>
2017-12-19 20:17:16 +1100

    increase version number

--- jobs/synthesis/synthesis-gesis-with-orcid-graph.sh
+++ jobs/synthesis/synthesis-gesis-with-orcid-graph.sh
@@ -1,6 +1,7 @@
 echo "Get the Synthesis Program..."
 cp -r $JENKINS_HOME/jobs/Build-Switchboard-Synthesis/workspace/target/* .
 
-JARFILE=sync-1.1.1.jar
+JARFILE=sync-1.1.2.jar
 
 java -jar $JARFILE synthesis-base.properties
+
