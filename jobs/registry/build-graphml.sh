cc90471a5ca578272ee3db041825ebb4d1de4032
Amir Aryani <amir.aryani@gmail.com>
2017-12-19 22:02:30 +1100

    added new graphml files

---
+++ jobs/registry/build-graphml.sh
@@ -0,0 +1,17 @@
+MNTDATA=$1
+SOURCE=$2
+
+rm -rf ./R.Neo4j
+cp -r $MNTDATA/$SOURCE/R.Neo4j/. ./R.Neo4j
+
+cd ./R.Neo4j
+
+./bin/neo4j start
+sleep 25s
+
+./bin/neo4j status
+
+echo "Exporting GraphML using Neo4j-Shell"
+./bin/neo4j-shell  -v -c -file  ../build-graphml-$SOURCE.cli
+
+./bin/neo4j stop
