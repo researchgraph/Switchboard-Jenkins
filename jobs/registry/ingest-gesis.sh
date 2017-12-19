cfefbaeebb2e6745f209e45509b47cf6e86abf00
Amir Aryani <amir.aryani@gmail.com>
2017-12-19 20:44:12 +1100

    added import to Neo4j

--- jobs/registry/ingest-gesis.sh
+++ jobs/registry/ingest-gesis.sh
@@ -1,6 +1,7 @@
 RNEO4J=$1
 MNTDATA=$2
-SOURCE=gesis
+CSVFOLDER=$3 
+SOURCE=#4
 
 echo "Create clean Neo4j Folder..."
 rm -rf $MNTDATA/$SOURCE/R.Neo4j
@@ -10,3 +11,26 @@ cp -r $RNEO4J/. $MNTDATA/$SOURCE/R.Neo4j
 aws s3 --region eu-west-2 cp  s3://$SOURCE.registry.researchgraph/csv-$SOURCE.tar.gz .
 tar xvzf csv-$SOURCE.tar.gz
 
+echo "Import into Neo4j"
+NODE_FILE_PATH=$CSVFOLDER"/link_db_entities.csv"
+NODE_FILE_PATH1=$CSVFOLDER"/link_db_entities_dataset.csv"
+NODE_FILE_PATH2=$CSVFOLDER"/link_db_entities_institution.csv"
+NODE_FILE_PATH3=$CSVFOLDER"/link_db_entities_project.csv"
+NODE_FILE_PATH4=$CSVFOLDER"/link_db_entities_publication.csv"
+NODE_FILE_PATH5=$CSVFOLDER"/link_db_entities_citedData.csv"
+NODE_FILE_PATH6=$CSVFOLDER"/link_db_entities_instrument.csv"
+RELATIONSHIP_FILE_PATH=$CSVFOLDER"/link_db_relationships.csv"
+IMPORT_SCRIPT=$MNTDATA/$SOURCE"/R.Neo4j/bin/neo4j-import"
+$DATABASE_PATH=$MNTDATA/$SOURCE"/R.Neo4j/data/databases/graph.db"
+
+$IMPORT_SCRIPT --into $DATABASE_PATH \
+	--nodes $NODE_FILE_PATH1 \
+	--nodes $NODE_FILE_PATH2 \
+	--nodes $NODE_FILE_PATH3 \
+	--nodes $NODE_FILE_PATH4 \
+	--nodes $NODE_FILE_PATH5 \
+	--nodes $NODE_FILE_PATH6 \
+	--relationships $RELATIONSHIP_FILE_PATH
+
+echo "Invalid relationships"
+cat $MNTDATA/$SOURCE/R.Neo4j/data/databases/graph.db/bad.log
